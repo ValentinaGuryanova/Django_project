c9e5d75842247d90212b09db8d810e2bcf1a4721
ValentinaGuryanova <gvalushka@mail.ru>
2023-10-16 00:28:18 +0300

    Урок 20.1 Работа с ORM в Django.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name', 'description',)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(max_length=500, verbose_name='описание', **NULLABLE)
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.CharField(max_length=100, verbose_name='категория')
+    #category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена за покупку')
+    data_create = models.DateField(auto_now_add=True, verbose_name='дата создания',)
+    data_last_change = models.DateField(auto_now=True, verbose_name='дата последнего изменения',)
+
+    def __str__(self):
+        return f'{self.name} {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('price',)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(max_length=500, verbose_name='описание', **NULLABLE)
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('name',)
