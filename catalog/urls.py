fa45047438f0b33ae6db49732e4993bad914f20b
ValentinaGuryanova <gvalushka@mail.ru>
2023-10-25 01:45:44 +0300

    Урок 21.1 FBV и CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, home, product
+from catalog.views import contacts, home, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
 
 app_name = CatalogConfig.name
+
 
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('product/', product, name='product')
+    path('product/', ProductListView.as_view(), name='product'),
+    path('create', ProductCreateView.as_view(), name='create'),
+    path('view/<int:pk>/', ProductDetailView.as_view(), name='view_product'),
+    path('edit/<int:pk>/', ProductUpdateView.as_view(), name='edit'),
+    path('delete/<int:pk>/', ProductDeleteView.as_view(), name='delete')
 ]
